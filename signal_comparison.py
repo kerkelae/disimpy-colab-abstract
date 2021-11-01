282fa09d4996e9469f59d87e8f4ca8fa7411bfa2
kerkelae <leevi.kerkela@protonmail.com>
2021-11-01 15:48:04 +0000

    changed markerstyle

--- signal_comparison.py
+++ signal_comparison.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 plt.rcParams["font.family"] = "serif"
 
-n_walkers = int(1e3)
+n_walkers = int(1e4)
 bs = np.linspace(1, 5e9, 11) / 1e9
 camino_signals = np.fromfile(
     f"results/camino_signals_w{n_walkers}_t10000.bfloat", ">f4"
@@ -11,8 +11,8 @@ camino_signals = np.fromfile(
 disimpy_signals = np.loadtxt(f"results/disimpy_signals_w{n_walkers}_t10000.txt")
 
 fig, ax = plt.subplots(1, figsize=(7, 4))
-ax.scatter(bs, disimpy_signals / n_walkers, color="tab:green", marker="s")
-ax.scatter(bs, camino_signals / n_walkers, color="tab:blue", marker="+", s=200)
+ax.scatter(bs, disimpy_signals / n_walkers, color="tab:green")
+ax.scatter(bs, camino_signals / n_walkers, color="tab:blue", marker="x", s=100)
 ax.set_ylabel("S/S$_0$")
 ax.set_xlabel("b (ms/μm$^2$)")
 ax.set_xticks(bs)
